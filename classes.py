47cc211f6939958cd9407484ab306cf6ee5a94e3
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-03-26 14:04:35 +0100

    reworked normalizing, added a bias neuron instead of a bias property for training purposes

--- classes.py
+++ classes.py
@@ -1,27 +1,40 @@
+import imp
 import math
 from os import stat
 import random as rnd
 from re import T
 import numpy as np
 import helpers as hlp
+import pandas as pd
 
 
 class Neuron:
     def __init__(self, id):
         self.id = id
-        self.output = 0
-        self.bias = 0 if self.id[0] == 0 else round((rnd.random() - 0.5) * 2, 2) # if layer id is 0, it is an input layer
+        self.output = 0 if self.id[1] > 0 else 1 # bias neurons have id 0 and get an output of 1
     
-    def propagate(self, prev_layer_neurons, current_layer_weights, current_layer_activation_function):
-        net_input = 0
+    def propagate(self, prev_layer, current_layer):
+        # input neurons and bias neurons should not propagate
+        if (self.id[0] == 0 or self.id[1] == 0):
+            return
 
-        for n in prev_layer_neurons:
-            net_input += n.output * current_layer_weights[n.id[1]][self.id[1]]
-            
-        net_input -= self.bias
-        self.output = round(self.activate(net_input, current_layer_activation_function), 2)
+        net_input = 0
+        prev_neurons = []
+        prev_neurons = prev_layer.neurons.copy()
+        prev_neurons.insert(0,current_layer.bias_neuron)
+
+        for n in prev_neurons:
+            try:
+                net_input += n.output * current_layer.weights[n.id[1]][self.id[1] - 1]
+            except:
+                print('Error i = ' + str(self.id) + ' j = ' + str(n.id) + ' w = ' + str(current_layer.weights[n.id[1]][self.id[1] - 1]))
+
+        self.output = round(self.activate(net_input, current_layer.activation), 2)
 
     def activate(self, input, activation):
+        if (self.id[1] == 0):
+            return 1
+
         match activation:
             case 'identity':
                 return input
@@ -42,20 +55,34 @@ class Layer:
         self.activation = activation if activation in ['identity', 'relu', 'binary_step', 'sigmoid', 'tanh'] else 'identity'
         self.neurons = []
 
+        # add bias neuron
+        if self.id > 0:
+            self.bias_neuron = Neuron((self.id, 0))
+        else:
+            self.bias_neuron = None
+
         # add neurons at initialization
-        for i in range(self.num_of_neurons):
-            self.neurons.append(Neuron((self.id, i), ))
+        for i in range(1, self.num_of_neurons + 1): # Neuron 0 is always the bias neuron
+            self.neurons.append(Neuron((self.id, i)))
         
         # if hidden or output layer, add weights
         if self.id > 0:
-            self.weights = np.zeros((prev_layer.num_of_neurons, self.num_of_neurons))
+            self.weights = np.zeros((prev_layer.num_of_neurons + 1, self.num_of_neurons))
             
             for i in range(len(self.weights)):
                 for j in range(len(self.weights[0])):
-                    self.weights[i][j] = round(rnd.random(), 2)
+                    if i == 0:
+                        self.weights[i][j] = -round(rnd.random(), 2) # negative weights for bias neurons
+                    else:
+                        self.weights[i][j] = round(rnd.random(), 2) # positive weights for normal neurons
+        else:
+            self.weights = []
 
-    def add_neuron(self):
-        self.neurons.append(Neuron((self.id, len(self.neurons))))
+    def add_neuron(self, output):
+        self.neurons.append(Neuron((self.id, len(self.neurons + 1))))
+
+    def get_weights(self):
+        return pd.DataFrame(self.weights)
 
 
 class Network_Model:
@@ -76,19 +103,12 @@ class Network_Model:
             self.layers.append(Layer(len(self.layers), num_of_neurons, activation, self.layers[-1]))
         else:
             self.layers.append(Layer(len(self.layers), num_of_neurons, activation))
-
-    def plot_network(self):
-        for l in self.layers:
-            s = '|'
-            for n in l.neurons:
-                s += ' ' + str(n.id) + ', o = ' + str(n.output) + ', b = ' + str(n.bias) + ' |'
-            print(s)
     
-    def plot_network_pretty(self):
+    def plot_network(self):
         for l in self.layers:
             s = ''
             for n in l.neurons:
-                s += '<div style="border-style:outset; border-radius: 1ex; border-color: white; padding: 0.5ex; text-align: center; float: left; margin: 0.25ex; width: fit-content">'+ str(n.id) + '<br>output ' + str(n.output) + '<br>bias ' + str(n.bias) + '</div>'
+                s += '<div style="border-style:outset; border-radius: 1ex; border-color: white; padding: 0.5ex; text-align: center; float: left; margin: 0.25ex; width: fit-content">'+ str(n.id) + '<br>output ' + str(n.output) + '</div>'
             hlp.printmd(s)
 
     def train(self, train_df_x, train_df_y, val_df_x, val_df_y, mode = 'online', epochs = 10):
@@ -99,7 +119,6 @@ class Network_Model:
             return
 
         # Training
-
         train_data_x = train_df_x.values.tolist()
         train_data_y = train_df_y.values.tolist()
 
@@ -109,8 +128,6 @@ class Network_Model:
         # t ... teaching input with components t_i
         # E_p ... Error vector for training sample p
 
-        # return
-
         for e in range(1, epochs + 1):
             E_e = 0 # Cumulative error for the epoch
             for i,p in enumerate(train_data_x):
@@ -119,7 +136,7 @@ class Network_Model:
 
                 # specific error
                 t = train_data_y[i]
-                E_p = 0
+                E_p = 0 # Error for a specific training set
                 temp_sum = 0
                 for j,y_j in enumerate(y):
                     temp_1 = (y_j - (t[j] if type(t) == list else t))
@@ -131,45 +148,21 @@ class Network_Model:
                 layers = self.layers.copy()
                 layers.reverse()
 
-                delta_weights = []
-
-                for l, layer in enumerate(layers):
-                    if l != 0:
-                        for j, neuron_j in enumerate(layer.neurons):
-                            if l == layers[0].id:
-                                # compute del_j
-                                pass
-                            else:
-                                # compute del_j
-                                pass
-                                
-                            for i, neuron_i in enumerate(layers[l - 1].neurons):
-                                # compute del_w
-                                pass
-
-                for l, layer in enumerate(layers):
-                    if l != 0:
-                        for neuron_i in range(len(layer.neurons)):
-                            for neuron_j in range(len(layer[l - 1].neurons)):
-                                # update weights with del_w
-                                pass
-
     def predict(self, input: list):
         # write input to first layer
         if len(input) != len(self.layers[0].neurons):
             print('Number of input values does not match number of input neurons!')
             return
 
+        # write input to layer 0 neurons
         for i, n in enumerate(self.layers[0].neurons):
             n.output = input[i]
         
         # propagate
         for l in self.layers:
-            if l.id == 0:
-                continue # prevent input neurons from propagating
-
-            for n in l.neurons:
-                n.propagate(self.layers[l.id - 1].neurons, l.weights, l.activation)
+            if l.id != 0:
+                for n in l.neurons:
+                    n.propagate(self.layers[l.id - 1], l)
     
         output = []
         for n in self.layers[-1].neurons:
--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ import pandas as pd
 def printmd(string):
     display(Markdown(string)) 
 
-def normalize(df_orig: pd.DataFrame, upper_bound = 1, lower_bound = -1):
+def normalize(df_orig: pd.DataFrame, min_val, max_val, lower_bound = -1, upper_bound = 1):
 
     if type(df_orig) != pd.DataFrame:
         print('Data must be of type Pandas.Dataframe')
@@ -19,18 +19,14 @@ def normalize(df_orig: pd.DataFrame, upper_bound = 1, lower_bound = -1):
     data_transposed = list(map(list, zip(*data_not_norm)))
 
     for i, column in enumerate(data_transposed):
-        v_max = -float('inf')
-        v_min = float('inf')
-
-        for value in column:
-            if value > v_max: v_max = value
-            if value < v_min: v_min = value
-        
-        for j, value in enumerate(column): data_transposed[i][j] = (value - v_min) / (v_max - v_min) * (upper_bound - lower_bound) + lower_bound
+        for j, value in enumerate(column):
+            # https://www.baeldung.com/cs/normalizing-inputs-artificial-neural-network#:~:text=Batch%20Normalization,convergence%20of%20the%20training%20process.
+            data_transposed[i][j] = (value - min_val) / (max_val - min_val) * (upper_bound - lower_bound) + lower_bound
 
     data_norm= list(map(list, zip(*data_transposed)))
     df_norm = pd.DataFrame(data_norm, columns = df_not_norm.columns)
     return df_norm
+
 
 def split_training_val_data(df: pd.DataFrame, percentage_val_data = 0.2):
 
